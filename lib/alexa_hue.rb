04419d658f3d57082ffdc4c9d6ecfd572e354208
Kyle Lucas <kglucas93@gmail.com>
2016-03-17 16:01:01 -0700

    Clean up, thread everything

--- lib/alexa_hue.rb
+++ lib/alexa_hue.rb
@@ -11,9 +11,7 @@ require 'alexa_hue/hue/voice_parser'
 require 'alexa_hue/hue/helpers'
 require 'chronic_duration'
 
-
 LEVELS = {} ; [*1..10].each { |t| LEVELS[t.to_s ] = t.in_words }
-
 
 module Hue
   include Hue::Helpers
@@ -25,35 +23,32 @@ module Hue
     end
     
     def control_lights
-      if @echo_request.slots.brightness
-        LEVELS.keys.reverse_each { |level| @echo_request.slots.brightness.sub!(level, LEVELS[level]) } if @echo_request.slots.schedule.nil? 
-      end
-
-      if @echo_request.slots.saturation
-        LEVELS.keys.reverse_each { |level| @echo_request.slots.saturation.sub!(level, LEVELS[level]) } if @echo_request.slots.schedule.nil?
-      end
-
-      @echo_request.slots.to_h.each do |k,v| 
-        @string ||= ""
-        next unless v
-        if k == :scene || k == :alert
-          @string << "#{v.to_s} #{k.to_s}  "
-        elsif k == :lights || k == :modifier || k == :state
-          @string << "#{v.to_s}  "
-        elsif k == :savescene
-          @string << "save scene as #{v.to_s} "
-        elsif k == :flash
-          @string << "start long alert "
-        else
-          @string << "#{k.to_s} #{v.to_s}  "
+      Thread.start do 
+        [:brightness, :saturation].each do |attribute|
+          LEVELS.keys.reverse_each { |level| @echo_request.slots.send(attribute).sub!(level, LEVELS[level]) } if @echo_request.slots.send(attribute) && @echo_request.slots.schedule.nil? 
         end
-      end
-        
-      fix_schedule_syntax(@string)        
-      @string.sub!("color loop", "colorloop")
-      @string.strip!
 
-      
+        @echo_request.slots.to_h.each do |k,v| 
+          @string ||= ""
+          next unless v
+          if k == :scene || k == :alert
+            @string << "#{v.to_s} #{k.to_s}  "
+          elsif k == :lights || k == :modifier || k == :state
+            @string << "#{v.to_s}  "
+          elsif k == :savescene
+            @string << "save scene as #{v.to_s} "
+          elsif k == :flash
+            @string << "start long alert "
+          else
+            @string << "#{k.to_s} #{v.to_s}  "
+          end
+        end
+        
+        fix_schedule_syntax(@string)        
+        @string.sub!("color loop", "colorloop")
+        @string.strip!
+        switch.voice @string
+      end
       
       # if @echo_request.slots.lights.nil? && @echo_request.slots.scene.nil? && @echo_request.slots.savescene.nil?
       #   halt AlexaObjects::Response.new(end_session: false, spoken_response: "Please specify which light or lights you'd like to adjust. I'm ready to control the lights.").to_json
@@ -86,7 +81,6 @@ module Hue
       #    halt r.without_card.to_json
       #  end
       #end
-      Thread.start { switch.voice @string }
 
       return AlexaObjects::Response.new(spoken_response: "okay").to_json
     end
