9dd0bfc8fce06006ebe81f5cd4bc17ad75da11b3
Kyle Lucas <kglucas93@gmail.com>
2016-03-17 19:16:30 -0700

    Add check to prevent bug

--- lib/alexa_hue.rb
+++ lib/alexa_hue.rb
@@ -31,7 +31,7 @@ module Hue
         switch.voice @echo_request
       end
 
-      if (@echo_request.slots.lights.nil? && @echo_request.slots.scene.nil? && @echo_request.slots.savescene.nil?) || @echo_request.slots.lights&.scan(/light|lights/).empty?
+      if (@echo_request.slots.lights.nil? && @echo_request.slots.scene.nil? && @echo_request.slots.savescene.nil?) || (@echo_request.slots.lights && @echo_request.slots.lights.scan(/light|lights/).empty?)
         halt AlexaObjects::Response.new(end_session: false, spoken_response: "Please specify which light or lights you'd like to adjust. I'm ready to control the lights.").to_json
       end
 
